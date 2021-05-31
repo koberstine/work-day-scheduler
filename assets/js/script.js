fbeaaad6d8594eec5a2fa91e723b3f71d997345a
Hammurabi <koberstine@hotmail.com>
2021-05-30 21:21:44 -0400

    formatting complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,43 @@ var scheduleArray = ["","","","","","","","","","","","","","","","","",""];
 $("#currentDay").text(dateToday);
 
 for (i=9; i<=17; i++) {
+    // one row
     $("<div/>", {
         id: i,
-        "class": "row",
-        "class": "time-block"
+        "class": "row time-block",
     }).appendTo(".container");
-    $("<div/", {
-        "class": "col-1",
-        "class": "hour",
-        text: i
+    // time column
+    $("<div/>", {
+        "class": "col-1 hour",
+        text: moment(i, "H").format("hA")
     }).appendTo("#"+i);
-    $("<div/", {
-        "class": "col-10"
+    // color code event column
+    if (i<currentTime) {
+        $("<div/>", {
+            //id: "event"+i,
+            "class": "col-10 past"
+        }).appendTo("#"+i);
+    } else if (i>currentTime) {
+        $("<div/>", {
+            //id: "event"+i,
+            "class": "col-10 future" 
+        }).appendTo("#"+i);
+    } else {
+        $("<div/>", {
+            //id: "event"+i,
+            "class": "col-10 present"
+        }).appendTo("#"+i);
+    }
+ //   $("<input/>", {
+ //       "type": "text"
+ //   }).appendTo("#event"+i)
+    // save button
+    $("<div/>", {
+        id: "save"+i,
+        "class": "col-1 saveBtn"
     }).appendTo("#"+i);
-    $("<div/", {
-        "class": "col-1",
-        "class": "saveBtn"
-    }).appendTo("#"+i);
-
+    $("<i/>", {
+        "class": "fas fa-save"
+    }).appendTo("#save"+i);
 }
+
